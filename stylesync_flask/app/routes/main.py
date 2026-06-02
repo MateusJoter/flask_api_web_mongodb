4457d326d98b396601ae0801a4fcb4cea7fe1082
Mateus Joter <mateusjoter17@gmail.com>
2026-06-02 09:51:10 -0300

    Implement user login with validation and error handling
    Add user authentication and error handling in login route.

--- stylesync_flask/app/routes/main.py
+++ stylesync_flask/app/routes/main.py
@@ -1,11 +1,23 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from pydantic import ValidationError
+from app.models.user import LoginPayload
 
 main_bp = Blueprint('main_bp', __name__)
 
 # RF: O sistema deve autenticar um usuário para que ele obtenha um token.
 @main_bp.route('/login', methods=['POST'])
 def login():
-    return jsonify({"message": "Rota de login."})
+    try:
+        raw_data = request.get_json()
+        user_data = LoginPayload(**raw_data) # O operador ** é o que desacopla o dicionário, transformando suas keys 
+                                             # em argumentos que possam ser validados pelo Loginpayload
+        except ValidationError as e:
+            return jsonify({"erro": e.errors()}), 400
+        except Exception as e:
+            return jsonify({"erro": f"Erro durante aa requisição do dado: {e}"}), 500
+            
+    return jsonify({"message": r"Realizar o login do usuário {user_data.model_dump_json()}."}) # model_dump_json() é a função responsável por transformar as
+                                                                                               # informações da classe Loginpayload de volta para um json.
 
 # RF: O sistema deve listar todos os produtos.
 @main_bp.route('/products', methods=['GET'])
@@ -34,7 +46,7 @@ def delete_product_by_id(product_id):
     
 # RF: O sistema deve permitir a importação de vendas através de um arquivo.
 @main_bp.route('/sales', methods='POST')
-def upload_sales(arquivo_vendas):
+def upload_sales():
     return jsonify({"message": "Rota de importação de vendas via arquivo."})
 
 
