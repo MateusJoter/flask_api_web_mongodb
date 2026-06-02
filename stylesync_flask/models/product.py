413a45dd783068cbd370ba5e63f76c7e3e730b4f
Mateus Joter <mateusjoter17@gmail.com>
2026-06-02 10:06:16 -0300

    Add 'id' field to Products model

--- stylesync_flask/models/product.py
+++ stylesync_flask/models/product.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 from typing import Optional # Nos dá a possibilidade de dizer se tal atributo é opcional
 
 class Products(BaseModel):
+    id: int
     name: str
     price: float
     description: Optional[str] = None
