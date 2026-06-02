9a1a57a1068040adf2407490beecc906c856d894
Mateus Joter <mateusjoter17@gmail.com>
2026-06-02 08:49:04 -0300

    Add Products model with attributes using Pydantic

---
+++ models/products.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+from typing import Optional # Nos dá a possibilidade de dizer se tal atributo é opcional
+
+class Products(BaseModel):
+    name: str
+    price: float
+    description: Optional[str] = None
+    stock: int
