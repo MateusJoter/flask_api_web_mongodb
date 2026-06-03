474c2d3cbc04fe7c11b2ac06510a66ad5bf2a816
Mateus Joter <mateusjoter17@gmail.com>
2026-06-03 09:00:35 -0300

    Update config import path in app initialization

--- stylesync_flask/app/__init__.py
+++ stylesync_flask/app/__init__.py
@@ -1,12 +1,11 @@
 from flask import Flask
 from pymongo import MongoClient
-from .config import Config
 
 db = None
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object('config.Config')
     global db
 
     try:
