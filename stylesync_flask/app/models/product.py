206f56a12d4e63d367ce824dc79dbcabb7d1c480
Mateus Joter <mateusjoter17@gmail.com>
2026-06-03 09:55:52 -0300

    Update Products model to use ObjectId and add serialization
    Refactor Products model to use ObjectId and add ProductDBModel for custom serialization.

--- stylesync_flask/app/models/product.py
+++ stylesync_flask/app/models/product.py
@@ -1,10 +1,24 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, ConfigDict
 from typing import Optional # Nos dá a possibilidade de dizer se tal atributo é opcional
+from bson import ObjectId
+
 
 class Products(BaseModel):
-    id: int
+    id: Optional[Objectid] = Field(None, alias='_id') # O alias serve para receber e enviar '_id', mas utilizar 'id' no tratamento interno
     name: str
     price: float
     description: Optional[str] = None
     stock: int
     sku: str # Stock Keeping Unit
+
+    model_config = ConfigDict(
+        populate_by_name=True, # Permite que o modelo seja populado utilizando o alias='_id'
+        arbitrary_types_allowed=True # Permite utilizar o ObjectId no Optional
+    )
+
+class ProductDBModel(Products):
+    def model_dump(self, by_alias: bool = False, exclude: Optional[set] = None):
+        data = super().model_dump(by_alias=by_alias, exclude=exclude)
+        if self.id:
+            data['_id'] = str(data['_id'])
+        return data
